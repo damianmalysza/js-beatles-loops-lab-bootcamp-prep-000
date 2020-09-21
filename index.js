d5ecde7c614fc870199e21469d4b0f293ed7fcf6
Damian Malysza <damian.malysza@gmail.com>
2020-09-21 01:50:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // add solution here
-var 
 
-function theBeatlesPlay(musician,instruments){
+function theBeatlesPlay(musicians,instruments){
+  var array=[]
+  for (let i=0;i<musicians.length();i++);
   
 }
